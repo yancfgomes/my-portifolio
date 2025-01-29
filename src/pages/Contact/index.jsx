2d50497cc6a6a394c5d28cfe6ce74855b0425b25
Yan da Costa Fernandes Gomes <143640083+yancfgomes@users.noreply.github.com>
2025-01-28 22:22:14 -0400

    add Contact page with icons and styles; implement contact section for user engagement

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,13 +1,53 @@
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Container from "../../components/Container/Container";
+import styles from "./Contact.module.css";
+import { GoMail } from "react-icons/go";
+import { BsWhatsapp, BsGithub, BsLinkedin } from "react-icons/bs";
 
 function Contact() {
   return (
     <>
       <Header />
       <Container>
-        <h2>Contact</h2>
+        <section className={styles.contact}>
+          <h2>Contact</h2>
+          <h3>Get in touch</h3>
+          <p>So we can talk more about it.</p>
+
+          <div className={styles.icons}>
+            <a
+              href="mailto:yancfgomes@gmail.com"
+              target="_blank"
+              rel="noopener noreffer"
+            >
+              <GoMail className={styles.icon} />
+            </a>
+            <a
+              href="https://wa.me/5592984267992"
+              target="_blank"
+              rel="noopener noreffer"
+            >
+              <BsWhatsapp className={styles.icon} />
+            </a>
+            <a
+              href="https://github.com/yancfgomes"
+              target="_blank"
+              rel="noopener noreffer"
+            >
+              <BsGithub className={styles.icon} />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/yancfgomes/"
+              target="_blank"
+              rel="noopener noreffer"
+            >
+              <BsLinkedin className={styles.icon} />
+            </a>
+            
+            
+          </div>
+        </section>
       </Container>
       <Footer />
     </>
