2658982eea70731fe5134e8019f976e00fde264e
Yan da Costa Fernandes Gomes <143640083+yancfgomes@users.noreply.github.com>
2025-02-02 18:21:47 -0400

    fix: correct image source path in Home component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,29 +1,28 @@
 import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
 
-
 function Home() {
   return (
-        <section className={styles.home}>
-          <div className={styles.apresentation}>
-            <p>
-              Hi, I am <br />
-              <span>Yan Fernandes</span> <br />
-              Dev Full Stack Jr.
-            </p>
-            <Link to="/about" className={` ${styles.btn} ${styles.btn_red} `} >
-              Discover More About Me
-            </Link>
-          </div>
+    <section className={styles.home}>
+      <div className={styles.apresentation}>
+        <p>
+          Hi, I am <br />
+          <span>Yan Fernandes</span> <br />
+          Dev Full Stack Jr.
+        </p>
+        <Link to="/about" className={` ${styles.btn} ${styles.btn_red} `}>
+          Discover More About Me
+        </Link>
+      </div>
 
-          <figure>
-            <img
-              className={styles.img_home}
-              src="\undraw_developer-activity_dn7p.svg"
-              alt="home image"
-            />
-          </figure>
-        </section>
+      <figure>
+        <img
+          className={styles.img_home}
+          src="/undraw_developer-activity_dn7p.svg"
+          alt="home image"
+        />
+      </figure>
+    </section>
   );
 }
 
