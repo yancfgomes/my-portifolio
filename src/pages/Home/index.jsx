2eb979c573b290f88f8aafbac5ad79e5c4addadc
Yan da Costa Fernandes Gomes <143640083+yancfgomes@users.noreply.github.com>
2025-02-02 18:19:43 -0400

    fix: correct image source path for Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -19,7 +19,7 @@ function Home() {
           <figure>
             <img
               className={styles.img_home}
-              src="public\undraw_developer-activity_dn7p.svg"
+              src="\undraw_developer-activity_dn7p.svg"
               alt="home image"
             />
           </figure>
