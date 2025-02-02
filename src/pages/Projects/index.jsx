3033e96c1cc50bd94735885226b0b8e97b288ff8
Yan da Costa Fernandes Gomes <143640083+yancfgomes@users.noreply.github.com>
2025-02-02 17:51:04 -0400

    add Projects page; implement repository fetching and card display

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,8 +1,39 @@
+import { useState, useEffect } from "react";
+import Card from "../../components/Cards";
+import styles from "./Projects.module.css";
+
 function Projects() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    const buscarRepositorios = async () => {
+      const response = await fetch(
+        "https://api.github.com/users/yancfgomes/repos?page=1&per_page=50"
+      );
+      const data = await response.json();
+      setRepositories(data);
+    };
+    buscarRepositorios();
+  }, []);
+
   return (
-    <>
-        <h2>Projects</h2>
-    </>
+    <section className={styles.projects}>
+      <h2>Projects</h2>
+      {repositories.length > 0 ? (
+        <section className={styles.list}>
+          {repositories.map((repo) => (
+            <Card
+              key={repo.id}
+              name={repo.name}
+              description={repo.description}
+              html_url={repo.html_url}
+            />
+          ))}
+        </section>
+      ) : (
+        <p>Carregando repositórios...</p>
+      )}
+    </section>
   );
 }
 
