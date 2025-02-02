6f23cd656c8fba19ee3f7416788aa70afd683b9f
Yan da Costa Fernandes Gomes <143640083+yancfgomes@users.noreply.github.com>
2025-02-02 17:56:15 -0400

    fix: update Card component to use anchor tag for external links and improve formatting

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,27 +1,31 @@
-import styles from './Cards.module.css'
-import { FaHtml5, FaCss3Alt, FaJs, FaReact } from 'react-icons/fa'
-import { BsArrowRight } from 'react-icons/bs'
-import { Link } from 'react-router-dom'
+import styles from "./Cards.module.css";
+import { FaHtml5, FaCss3Alt, FaJs, FaReact } from "react-icons/fa";
+import { BsArrowRight } from "react-icons/bs";
 
 // eslint-disable-next-line react/prop-types
 function Card({ name, description, html_url }) {
-    return (
-        <section className={styles.card}>
-            <h3>{name}</h3>
-            <p>{description}</p>
-            <div className={styles.card_footer}>
-                <div className={styles.card_icones}>
-                    <FaHtml5 />
-                    <FaCss3Alt />
-                    <FaJs />
-                    <FaReact />
-                </div>
-                <Link to={html_url} className={styles.botao}>
-                    <BsArrowRight />
-                </Link>
-            </div>
-        </section>
-    )
+  return (
+    <section className={styles.card}>
+      <h3>{name}</h3>
+      <p>{description}</p>
+      <div className={styles.card_footer}>
+        <div className={styles.card_icones}>
+          <FaHtml5 />
+          <FaCss3Alt />
+          <FaJs />
+          <FaReact />
+        </div>
+        <a
+          href={html_url}
+          target="_blank"
+          rel="nopenner noreferrer"
+          className={styles.botao}
+        >
+          <BsArrowRight />
+        </a>
+      </div>
+    </section>
+  );
 }
 
-export default Card
+export default Card;
