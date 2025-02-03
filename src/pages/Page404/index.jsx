eb15078de60d361fd485e1329661f99c4ac551e9
Yan da Costa Fernandes Gomes <143640083+yancfgomes@users.noreply.github.com>
2025-02-03 18:09:15 -0400

    fix: enhance responsiveness across various components and pages with media queries

--- src/pages/Page404/index.jsx
+++ src/pages/Page404/index.jsx
@@ -2,13 +2,13 @@ import styles from "./Page404.module.css";
 
 function Page404() {
   return (
-    <>
-        <h2 className={styles.title2} >Ops!🫤</h2>
-        <div className={styles.txts}>
-          <span className={styles.big_txt}>404</span>
-          <strong className={styles.red_txt}>Page Not Found</strong>
-        </div>
-    </>
+    <section className={styles.page404}>
+      <h2 className={styles.title2}>Ops!🫤</h2>
+      <div className={styles.txts}>
+        <span className={styles.big_txt}>404</span>
+        <strong className={styles.red_txt}>Page Not Found</strong>
+      </div>
+    </section>
   );
 }
 
